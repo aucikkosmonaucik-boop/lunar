1a505c54d4077cc6e0d90eeecd3d48ba083a44a4
aucikkosmonaucik-boop <aucikkosmonaucik@gmail.com>
2026-03-12 11:20:58 +0100

    start

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ShoppingBag, User, Search, Menu, X } from 'lucide-react';
 import { useCart } from '../../context/CartContext';
