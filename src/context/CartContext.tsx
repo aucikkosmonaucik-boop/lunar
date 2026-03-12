098c61b8b86d6ba691a4eeca19a90f9db140dcfd
aucikkosmonaucik-boop <aucikkosmonaucik@gmail.com>
2026-03-12 11:15:49 +0100

    start strony dev

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useCallback } from 'react';
-import { Product, CartItem } from '../types';
+import type { Product, CartItem } from '../types';
 
 interface CartContextType {
   items: CartItem[];
