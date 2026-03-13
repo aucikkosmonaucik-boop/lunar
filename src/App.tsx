a9652a030b025ce3076e29d3c84e61ad9d07851b
aucikkosmonaucik-boop <aucikkosmonaucik@gmail.com>
2026-03-13 10:22:18 +0100

    account

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
           <Router>
             <div className="flex flex-col min-h-screen bg-white text-wonders-dark">
             <Navbar />
-            <main className="flex-grow pt-[320px]">
+            <main className="flex-grow" style={{ paddingTop: '450px' }}>
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/sklep" element={<ShopPage />} />
