1b08f478293235aa65a4b030caa29c836d436ff1
aucikkosmonaucik-boop <aucikkosmonaucik@gmail.com>
2026-03-13 10:13:37 +0100

    panel?

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
           <Router>
             <div className="flex flex-col min-h-screen bg-white text-wonders-dark">
             <Navbar />
-            <main className="flex-grow pt-[210px]">
+            <main className="flex-grow pt-[320px]">
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/sklep" element={<ShopPage />} />
